5f9933f194e83fcecfc820f87d0d966c4e3637ca
Noah <noahtullysanderson@gmail.com>
2018-02-04 23:05:20 -0500

    fixed some stuff, added content

---
+++ Assets/Javascript/app.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/4.9.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB06DOUHy-tNALeCpreH7eT0NCQ2POWpFc",
+    authDomain: "train-schedule-97071.firebaseapp.com",
+    databaseURL: "https://train-schedule-97071.firebaseio.com",
+    projectId: "train-schedule-97071",
+    storageBucket: "",
+    messagingSenderId: "162917514403"
+  };
+  firebase.initializeApp(config);
+</script>
